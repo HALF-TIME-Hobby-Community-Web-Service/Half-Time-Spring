a27f56b35d4a24bdc887d5e32efafe678456c996
Gyuchul Lee <lso3548@naver.com>
2023-09-18 11:41:49 +0900

    099

--- src/main/webapp/resources/js/join2.js
+++ src/main/webapp/resources/js/join2.js
@@ -54,25 +54,28 @@ $(function () {
   $(formObj).submit(function (event) {
     event.preventDefault(); // 폼 전송 방지
     $.ajax({
-      url: 'http://localhost:8888/hf/join2',
+      url: 'http://localhost:8888/user/join2',
       method: 'POST',
+      dataType: "JSON",
       data: {
         userid: userid,
-        userlocation: location.val(),
-        usercategory: category.val(),
+        location: location.val(),
+        category: category.val(),
       },
 
       //여기부터 다시하기
       success: function (response) {
+      
         alert('회원가입 완료');
-        if (response == '2') {
-          window.location.href = 'navbar.html'; //임시로해놓음 (회원가입 완료 후 피드페이지 이동)
+        alert(response);
+        if (response == 2) {
+          window.location.href = 'http://localhost:8888/user/login'; //임시로해놓음 (회원가입 완료 후 피드페이지 이동)
         } else {
           alert('다시작성해주세요');
         }
       },
       error: function (jqXhr, status) {
-        alert(`실패: ${status}\n오류명:${jqXhr.statusCode}`);
+        alert(`실패: ${status}\n오류명:${jqXhr.status}`);
       },
     });
     return false;
