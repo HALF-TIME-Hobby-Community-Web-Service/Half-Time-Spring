2627baef609c529c3d284743ee64936d96a3c0f0
Gyuchul Lee <lso3548@naver.com>
2023-09-18 19:42:45 +0900

    오류 수정

--- src/main/java/com/hf/mapper/UserMapper.java
+++ src/main/java/com/hf/mapper/UserMapper.java
@@ -12,7 +12,7 @@ public interface UserMapper {
     void insertUserLocation(@Param("id") String id , @Param("location") String location);
     void insertUserCategory(@Param("id") String id, @Param("category") String category);
     int joinIDcheckpage(String id);
-<<<<<<< HEAD
+
 	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
 	String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
 	String LoginFindPW(@Param("id") String id, @Param("name") String name,
@@ -20,15 +20,5 @@ public interface UserMapper {
 						@Param("pnum") String pnum);
 	String loginfindPW_IDCheck(String id);
 	
-=======
 
-	public void insertUser(User user);
-    public void insertUserLocation(@Param("id") String id , @Param("location") String location);
-    public void insertUserCategory(@Param("id") String id, @Param("category") String category);
-
-    public int joinIDcheckpage(String id);
-
-	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
-	public String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
->>>>>>> 6fe0d743ed308403c7a8c502387e8954af0458b6
 }
