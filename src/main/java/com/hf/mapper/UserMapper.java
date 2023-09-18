dac09d01bb6d9dca515eb42f53c0b88cd3096daf
Hward <sm9270000@gmail.com>
2023-09-18 16:45:53 +0900

    4:45 09-18 성민

--- src/main/java/com/hf/mapper/UserMapper.java
+++ src/main/java/com/hf/mapper/UserMapper.java
@@ -12,10 +12,5 @@ public interface UserMapper {
     void insertUserCategory(@Param("id") String id, @Param("category") String category);
     int joinIDcheckpage(String id);
 	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
-<<<<<<< HEAD
-	String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
-=======
-	
-
->>>>>>> b45b4bfe428e04d38cd4ecb34b0cebdcad85ad5f
+	public String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
 }
