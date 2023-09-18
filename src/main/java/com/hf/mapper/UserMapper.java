375aa194ab8fe1f5c34dabae2c7ba5b3f2a275ee
bomi <johndoe@example.com>
2023-09-18 16:38:20 +0900

    user 병합오류 수정

--- src/main/java/com/hf/mapper/UserMapper.java
+++ src/main/java/com/hf/mapper/UserMapper.java
@@ -10,11 +10,8 @@ public interface UserMapper {
 	public void insertUser(User user);
     public void insertUserLocation(@Param("id") String id , @Param("location") String location);
     public void insertUserCategory(@Param("id") String id, @Param("category") String category);
-<<<<<<< HEAD
+
     public int joinIDcheckpage(String id);
-=======
-    int joinIDcheckpage(String id);
->>>>>>> a736a2e97b393ada1aa0eadd075220a5af6c0ff3
 	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
 	public String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
 }
