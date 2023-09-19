37b2ab34362d06828f5650524f78606387d9ae61
Gyuchul Lee <lso3548@naver.com>
2023-09-19 09:38:47 +0900

    오류수정

--- src/main/java/com/hf/mapper/UserMapper.java
+++ src/main/java/com/hf/mapper/UserMapper.java
@@ -8,28 +8,17 @@ import com.hf.domain.User;
 @Component
 public interface UserMapper {
 
-<<<<<<< HEAD
-	public void insertUser(User user);
-    public void insertUserLocation(@Param("id") String id , @Param("location") String location);
-    public void insertUserCategory(@Param("id") String id, @Param("category") String category);
-
-    public int joinIDcheckpage(String id);
+    	void insertUser(User user);
+    	void insertUserLocation(@Param("id") String id , @Param("location") String location);
+    	void insertUserCategory(@Param("id") String id, @Param("category") String category);
+    	int joinIDcheckpage(String id);
 
 	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
-	public String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
-=======
-    void insertUser(User user);
-    void insertUserLocation(@Param("id") String id , @Param("location") String location);
-    void insertUserCategory(@Param("id") String id, @Param("category") String category);
-    int joinIDcheckpage(String id);
-
-	public User userLogin(@Param("ID") String ID, @Param("PWD")String PWD);
-	String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
-	String LoginFindPW(@Param("id") String id, @Param("name") String name,
+		String LoginFindID(@Param("name") String name, @Param("pnum") String pnum);
+		String LoginFindPW(@Param("id") String id, @Param("name") String name,
 						@Param("gender") int gender, @Param("birth") String birth,
 						@Param("pnum") String pnum);
-	String loginfindPW_IDCheck(String id);
-	
->>>>>>> 2627baef609c529c3d284743ee64936d96a3c0f0
+		String loginfindPW_IDCheck(String id);
+
 
 }
